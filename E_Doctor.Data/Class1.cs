ff7c34ed4bd114ffd595130c1e24835dddae9353
maarufdev <maarufdev1231@gmail.com>
2025-07-27 16:41:13 +0800

    added web application project

--- E_Doctor.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace E_Doctor.Data
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ E_Doctor.WindowsFormApp/Helpers/ConsoleHelper.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+namespace E_Doctor.WindowsFormApp.Helpers;
+public class ConsoleHelper
+{
+    [DllImport("kernel32.dll")]
+    public static extern bool AllocConsole();
+}
